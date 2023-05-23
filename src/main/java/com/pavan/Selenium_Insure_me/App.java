5ec5304391e2b7c917daeccac3651ccabb1dc6d8
pavankasiboina <pavankasiboina@gmail.com>
2023-05-23 16:51:21 +0530

    changes done

--- src/main/java/com/pavan/Selenium_Insure_me/App.java
+++ src/main/java/com/pavan/Selenium_Insure_me/App.java
@@ -10,7 +10,6 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 /**
